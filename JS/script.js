cbbfe901f1385b886f68a0277b7603831dcb1480
loic <loicanaelbdf@gmail.com>
2022-05-05 21:03:57 -0700

    quick changes

--- JS/script.js
+++ JS/script.js
@@ -11,18 +11,34 @@ console.log(allUstensils);
 
 let recipesList = document.getElementById("recipes");
 let input = document.querySelector(".search-input");
-let inputValue = input.value;
-inputValue = "";
+let inputValue = '';
+let recipeArray = [];
 
-function displayRecipes(recipes, inputValue) {
-  recipes.forEach((recipe) => {
+function displayRecipes(allRecipes, inputValue) {
+  recipesList.innerHTML = '';
+  allRecipes.forEach((recipe) => {
     if (inputValue == null || inputValue == undefined || inputValue == "") {
       let recipeTemplate = new RecipeCard().recipeCard(recipe);
       recipesList.innerHTML += recipeTemplate;
-    } else {
-      recipesList.innerHTML = "";
     }
     return recipesList;
   });
 }
+
 displayRecipes(allRecipes, inputValue);
+
+input.addEventListener('input', (e) => {
+  inputValue = e.target.value;
+  let inputLength = inputValue.length
+  if (inputLength >= 3) {
+    allRecipes.forEach((recipe) => {
+      if ((recipe.name.includes(inputValue) || recipe.description.includes(inputValue) || recipe.ingredients.some(a => a.ingredient.includes(inputValue)))) {
+        recipeArray.push(recipe);
+        console.log(recipe);
+      } else {
+        recipeArray = recipesList;
+      }
+    })
+  }
+  displayRecipes(recipeArray, inputValue);
+})
